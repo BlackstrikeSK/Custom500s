7d2e7b886d51b856412c6234f2c211b32fef152f
BlackstrikeSK <60195039+BlackstrikeSK@users.noreply.github.com>
2021-10-20 08:35:41 +0200

    Update ItemConfig.cs

--- Custom500s/Configs/ItemConfig.cs
+++ Custom500s/Configs/ItemConfig.cs
@@ -11,7 +11,7 @@ namespace Custom500s.Configs
         [Description("The list of Custom 500s")]
         public List<SCP330> SCP330s { get; private set; } = new List<SCP330>()
         {
-            new SCP330 {Type = ItemType.SCP500},
+            new SCP330() {Type = ItemType.SCP500},
         };
     }
 }
